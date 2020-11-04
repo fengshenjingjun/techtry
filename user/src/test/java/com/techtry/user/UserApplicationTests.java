8bdc2c703f5d9aa286f6aa20d5aa0ecc44915b9f
“lingxinyang <380904258@qq.com>
2020-11-04 19:21:06 +0800

    first commit

---
+++ user/src/test/java/com/techtry/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtry.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
